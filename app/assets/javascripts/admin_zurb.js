efc06255ee43f49811018c2b2bafc2d5fc8cee04
sgzhe <6485785@qq.com>
2018-11-14 16:53:10 +0800

    first

---
+++ app/assets/javascripts/admin_zurb.js
@@ -0,0 +1,6 @@
+//= require rails-ujs
+//= require turbolinks
+//= require foundation
+$(document).on('turbolinks:load', function() {
+    $(function(){ $(document).foundation(); });
+});
---
+++ app/controllers/admin/app_controller.rb
@@ -0,0 +1,5 @@
+module Admin
+  class AppController < ActionController::Base
+    layout 'admin_zurb'
+  end
+end
